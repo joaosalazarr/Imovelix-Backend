70ed842375313e37c551bd38b93d1e4ac2265e06
Voidzinn <voidboyyy@gmail.com>
2026-05-11 14:04:12 -0300

    Initial commit!

---
+++ src/main/java/com/api/imovelix/ImovelixApplication.java
@@ -0,0 +1,13 @@
+package com.api.imovelix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovelixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImovelixApplication.class, args);
+	}
+
+}
